f6102715667f9320379f99ebc9030877c469ceb8
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2018-11-28 17:58:09 -0500

    Updates

---
+++ precache-manifest.29e79883809cdd75212571b594a8dd7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d15f91c351667a6b6aea",
+    "url": "/chikipuntos/static/js/runtime~main.d15f91c3.js"
+  },
+  {
+    "revision": "e87691ca4a77db26e076",
+    "url": "/chikipuntos/static/js/main.e87691ca.chunk.js"
+  },
+  {
+    "revision": "7bfb90c803a1ab3288fa",
+    "url": "/chikipuntos/static/js/1.7bfb90c8.chunk.js"
+  },
+  {
+    "revision": "e87691ca4a77db26e076",
+    "url": "/chikipuntos/static/css/main.aa5513aa.chunk.css"
+  },
+  {
+    "revision": "7bfb90c803a1ab3288fa",
+    "url": "/chikipuntos/static/css/1.98cbb0aa.chunk.css"
+  },
+  {
+    "revision": "14b84b83f2082a32a1f5c16628c74fe6",
+    "url": "/chikipuntos/index.html"
+  }
+];
